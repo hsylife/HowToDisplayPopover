86ab89dbcc5d64e5859e02afcf980aa9b2ffd120
Y.T. Hoshino <ythshn@gmail.com>
2016-10-11 22:18:21 +0900

    display popover with UIPopoverController.

---
+++ HowToDisplayPopover/ContentViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ContentViewController.swift
+//  HowToDisplayPopover
+//
+//  Created by Y.T. Hoshino on 2016/10/11.
+//  Copyright © 2016年 Yuta Hoshino. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HowToDisplayPopover/ViewController.swift
+++ HowToDisplayPopover/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverControllerDelegate {
 
+    var popover: UIPopoverController?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapUIPopoverController(_ sender: UIButton) {
+        
+        //Only for iPad
+        if UIDevice.current.userInterfaceIdiom != .pad {
+            print("UIPopoverController is only for iPad.")
+            return
+        }
+        
+        //Prepare the instance of ContentViewController which is the content of popover.
+        let contentVC = ContentViewController()
+        //set size
+        contentVC.preferredContentSize = CGSize(width: 320, height: 280)
+        //make popover controller
+        popover = UIPopoverController(contentViewController: contentVC)
+        //set delegate
+        popover?.delegate = self
+        //present popover
+        popover?.present(from: sender.frame, in: view, permittedArrowDirections: .any, animated: true)
+    }
+    
+    //UIPopoverControllerDelegate
+    func popoverControllerDidDismissPopover(_ popoverController: UIPopoverController) {
+        popover = nil
+    }
+    
 }
 
